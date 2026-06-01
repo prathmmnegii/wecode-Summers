770656b9e51563a4091aa3240d74a291fe8b8e39
prathmmnegii <negipratham905@gmail.com>
2026-06-01 18:03:17 +0530

    Create sumOfDigits02.java

---
+++ Recursion 01/sumOfDigits02.java
@@ -0,0 +1,12 @@
+class sumOfDigits02{
+    static int summ(int num){
+        if(num==0){
+            return 0;
+        }
+        return (num%10) + summ(num/10);
+    }
+    public static void main(String[] args) {
+        System.out.println(summ(456));
+    }
+
+}
