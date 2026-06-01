6238e5b54f88ff7eeeb32cc6148dda730e51723b
prathmmnegii <negipratham905@gmail.com>
2026-06-01 14:28:45 +0530

    sum of digits using recursion 1

---
+++ Recursion 01/sumOfDigits01.java
@@ -0,0 +1,14 @@
+class sumOfDigits01{
+    static void summ(int num, int sum){
+        if(num==0){ // base case
+            System.out.println( sum); // sum ek hi bar print hoga (final output)
+            return;
+        }
+        int digit=num%10; // last digit extract kari
+        sum= sum+digit;   // sum mai digit add kari 
+        summ(num/10, sum); // num ko chota kiya 
+    }
+    public static void main(String[] args) {
+        summ(456, 0);
+    }
+}
