3c1e5858508629eb0aa532dafe8a0d056b0b0ee5
prathmmnegii <negipratham905@gmail.com>
2026-06-10 21:24:53 +0530

    Create odddevennn2.java

---
+++ Recursion 01/odddevennn2.java
@@ -0,0 +1,25 @@
+ class odddevennn{
+    static int[] evenodd(int n){
+        if(n==0){
+            return new int[2]; // evencount, oddcount
+        }
+        int [] result= evenodd(n-1); // small problem
+        if(n%2==0){
+            result[0]++;
+        }
+        else{
+            result[1]++;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        int n=10;
+        int[] result= evenodd(n);
+        System.out.println("Even " +result[0]);
+        System.out.println("Odd " + result[1]);
+    }
+}
+    
+
+    
+
