c1119c7892fda4f4dfbc6c1982e1991b5df40e24
prathmmnegii <negipratham905@gmail.com>
2026-07-02 17:48:08 +0530

    practice

--- LinkedList 01/linkedlist.java
+++ LinkedList 01/linkedlist.java
@@ -30,16 +30,103 @@
         return head;
     }
 
+    static void print(Node head){
+        Node temp= head;
+        while(temp!=null){
+            System.out.print(temp.val+" ");
+            temp=temp.next;
+        }
+    }
+
+    public static Node removeHead(Node head){
+        if(head==null){
+            return head;
+        }
+        Node temp = head;
+        head= head.next;
+        return head;
+    }
+
+    public static Node removetail(Node head){
+        if(head==null || head.next==null){
+            return null;
+        }
+        Node temp= head;
+        while(temp.next.next!= null){
+            temp=temp.next;
+        }
+        temp.next=null;
+        return head;
+    }
+
+    public static Node  removeposition(Node head, int k){
+        
+        if(head==null){
+            return head;
+        }
+
+        if(k==1){
+            Node temp =head;
+            head=head.next;
+            return head;
+        }
+       
+        int cnt=1;
+        Node temp= head;
+        Node prev = null;
+        while(temp!=null){      
+        if(cnt==k){
+            prev.next= prev.next.next;
+            break;
+
+        }
+        prev=temp;
+        temp=temp.next;
+        cnt++;
+    }
+    return head;
+
+    }
+
+    public static Node removeele(Node head, int ele){
+
+        if(head==null){
+            return head;
+        }
+        if(head.val==ele){
+            head= head.next;
+            return head;
+        }
+        Node temp= head; 
+        Node prev=null;
+
+        while(temp !=null){
+                    if(temp.val==ele){
+            prev.next=prev.next.next;  //prev.next=temp.next bhi likh skte hai
+            break;
+        }
+        prev=temp;
+        temp=temp.next;
+        }
+
+
+
+        return head;
+    }
+
+    /* Node temp=head;
+        while(temp!= null){
+            System.out.print(temp.val+" "); //traversal for linkled list
+            temp= temp.next;
+        } */
+
     public static void main(String[] args) {
         int [] arr= {2, 3, 4, 8};
 
         Node head= createll(arr);
-
-        Node temp=head;
-        while(temp!= null){
-            System.out.print(temp.val+" ");
-            temp= temp.next;
-        }
+        head= removeposition(head, 2);
         
+        print (head);
+  
     }
 }
